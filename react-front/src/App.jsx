b2b1efc60e198c55b055c930c9d157eecb881689
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-17 12:34:48 -0400

    Add nav template

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -1,11 +1,43 @@
+import {Link} from "react-router-dom";
+
 function App() {
 
 
-    return (
-        <div className="max-w-6xl mx-auto">
-            <h1>Welcome</h1>
-        </div>
-    )
+    return (<div className="max-w-6xl mx-auto">
+        <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+            <div
+                className="container mx-auto flex flex-wrap items-center justify-between"
+                bis_skin_checked="1">
+                <a href="#" className="flex items-center">
+                    Breeze-React
+                </a>
+
+                <div
+                    className="hidden w-full md:block md:w-auto">
+                    <ul className="mt-4 flex flex-col rounded-lg p-4 md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+                        <li>
+                            <Link to={"/"}
+                                  class="block rounded py-2 pr-4 pl-3 text-white"
+                                  aria-current="page">Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/login"}
+                                  class="block rounded py-2 pr-4 pl-3 text-white"
+                                  aria-current="page">Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/register"}
+                                  class="block rounded py-2 pr-4 pl-3 text-white"
+                                  aria-current="page">Register
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>)
 }
 
 export default App
