672be9b60f49a02774bbb26e9f507c66f272bed5
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-03-17 11:33:43 -0400

    Update App

--- react-front/src/App.jsx
+++ react-front/src/App.jsx
@@ -1,12 +1,11 @@
-
 function App() {
 
 
-  return (
-    <div className="App">
-
-    </div>
-  )
+    return (
+        <div className="max-w-6xl mx-auto">
+            <h1>Welcome</h1>
+        </div>
+    )
 }
 
 export default App
